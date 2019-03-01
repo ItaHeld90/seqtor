0914c417b75708a34107d1a0b2e711baad905c81
Itamar Held <ita@gmail.com>
2019-03-01 04:03:01 +0200

    piper consume receives an object

--- src/piper.ts
+++ src/piper.ts
@@ -1,21 +1,23 @@
-import { IPipeWrapper, PropKey } from "./general-types";
+import { IPipeWrapper, PropKey, IterableConsumer } from "./general-types";
 import { toArray, toMap, toSet, toES6Map } from "./intermideate-consumers";
+import { consumeIterable } from "./iterable-utils";
 
 export function pipeWrapper<T>(iterable: Iterable<T>): IPipeWrapper<T> {
-    return {
-        pipe: (...fns: any[]) => {
-            return pipeWrapper(
-                fns.reduce(
-                    (result, fn) => fn(result),
-                    iterable
-                )
-            )
-        },
-        consume: <R>(consumeFn: (iterable: Iterable<T>) => R) => consumeFn(iterable),
-        toArray: () => toArray<T>()(iterable),
-        toMap: <V>(keyValFn: (item: T) => [PropKey, V]) => toMap(keyValFn)(iterable),
-        toSet: () => toSet(iterable),
-        toES6Map: <K, V>(keyValFn: (item: T) => [K, V]) => toES6Map(keyValFn)(iterable),
-        value: iterable
-    }
+  return {
+    pipe: (...fns: Function[]) =>
+      pipeWrapper(fns.reduce((result, fn) => fn(result), iterable)),
+    consume: <R, S>(
+      consumer: ((iterable: Iterable<T>) => R) | IterableConsumer<R, T, S>
+    ) =>
+      typeof consumer === "function"
+        ? consumer(iterable)
+        : consumeIterable(consumer)(iterable),
+    toArray: () => toArray<T>()(iterable),
+    toMap: <V>(keyValFn: (item: T) => [PropKey, V]) =>
+      toMap(keyValFn)(iterable),
+    toSet: () => toSet(iterable),
+    toES6Map: <K, V>(keyValFn: (item: T) => [K, V]) =>
+      toES6Map(keyValFn)(iterable),
+    value: iterable
+  };
 }
